9d1bdfddef899e445aee2526f3dcb784b29c2f6b
Cer112 <46543271+Cer112@users.noreply.github.com>
2019-02-11 18:12:16 -0500

    update to retrieving unique data

--- monthly_sales.py
+++ monthly_sales.py
@@ -5,16 +5,15 @@
 # TODO: write some Python code here to produce the desired functionality...
 
 import os
-<<<<<<< HEAD
 import pandas
 
 ## ... adapted from: https://github.com/s2t2/shopping-cart-screencast/blob/30c2a2873a796b8766e9b9ae57a2764725ccc793/shopping_cart.py#L56-L59
 def to_usd(my_price):
     return "${0:,.2f]".format(my_price)
 
-CSV_FILENAME = "sales-201803.csv"
+csv_filename = "sales-201803.csv"
 
-csv_filepath = os.path.join(os.path.dirname(_file_), "data", csv_filename)
+csv_filepath = os.path.join(os.path.dirname(__file__), "data", csv_filename)
 
 csv_data = pandas.read_csv(csv_filepath)
 
@@ -39,31 +38,7 @@ breakpoint()
 top_sellers = [
     {"rank": 1, "name": "Button-Down Shirt", "monthly_sales": 6960.35},
     {"rank": 2, "name": "Super Soft Hoodie", "monthly_sales": 1875},
-=======
-import csv
-import itertools
-import operator import itemgetter
-
-CSV_FILENAME = "sales-201803.csv"
-
-csv_filepath = os.path.join("data", CSV_FILENAME)
-
-
-with open(csv_filepath, "r") as csv_file:
-    reader = csv.DictReader(csv_file)
-    for od in reader:
-        rows.append(dict(od)) 
-
-sales_price = [float(row["sales price"]) for row in rows] 
-total_sales = sum(sales_pries)
->>>>>>> b416c9189fcbadfab316b74a26824e492ea5e232
-
-
-
-
-
-
-
+]
 
 print("-----------------------")
 print("MONTH: March 2018")
