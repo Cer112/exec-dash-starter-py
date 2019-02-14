47b12dc974f5284c8084689eff85efea634f5fb3
Cer112 <46543271+Cer112@users.noreply.github.com>
2019-02-14 17:46:01 -0500

    Changed plotly chart method. Backtracked a couple steps. Now have top-selling products and a bar-chart

--- monthly_sales.py
+++ monthly_sales.py
@@ -1,18 +1,9 @@
 # monthly_sales.py
 
-#
-#loop through top sellers- last commit completed.
-#
-
-
-
-
 import os
 import pandas
-import plotly
-from plotly import graph_objs
+import matplotlib.pyplot as plt
 
-## ... adapted from: https://github.com/s2t2/shopping-cart-screencast/blob/30c2a2873a796b8766e9b9ae57a2764725ccc793/shopping_cart.py#L56-L59
 def to_usd(my_price):
     return "${0:,.2f}".format(my_price)
 
@@ -20,14 +11,11 @@ def to_usd(my_price):
 #Inputs
 #
 
-
-
 csv_filename = "sales-201803.csv"
 
 csv_filepath = os.path.join(os.path.dirname(__file__), "data", csv_filename)
 
 csv_data = pandas.read_csv(csv_filepath)
-
 
 #
 #Calculations
@@ -50,21 +38,6 @@ for i, row in product_totals.iterrows():
         top_sellers.append(d)
         rank = rank + 1
 
-
-#>                    unit price  units sold  sales price
-#> product
-#> Button-Down Shirt     1821.40         107      6960.35
-#> Super Soft Hoodie     1350.00          25      1875.00
-#> Khaki Pants           1157.00          18      1602.00
-#> Vintage Logo Tee       398.75          59       941.05
-#> Brown Boots            250.00           2       250.00
-#> Sticker Pack           108.00          48       216.00
-#> Baseball Cap           156.31           7       156.31
-
-
-
-
-
 print("-----------------------")
 print("MONTH: March 2018")
 
@@ -72,13 +45,14 @@ print("-----------------------")
 print("CRUNCHING THE DATA...")
 
 print("-----------------------")
-print("TOTAL MONTHLY SALES: $12,000.71")
+print("TOTAL MONTHLY SALES: {to_usd(monthly_total)}")
 
 print("-----------------------")
 print("TOP SELLING PRODUCTS:")
-for d in top_sellers:
-    print(" " + str(d["rank"]) + ") " + d["name"] + ": " + to_usd(d["monthly_sales"]))
 
+rank = 1
+for d in top_sellers:
+    print(" " + str(rank) + ") " + d["name"] + ": " + to_usd(d["monthly_sales"]))
 
 print("-----------------------")
 print("VISUALIZING THE DATA...")
@@ -88,32 +62,17 @@ print("VISUALIZING THE DATA...")
 #Data Visualization
 #
 
-chart_filename = "top-sellers-201803.html" #not finding this filname in plotly
-chart_filepath = os.path.join(os.path.dirname(__file__), "reports", chart_filename)
-
-data = [
-    graph_objs.Bar(
-        x=sorted_products_sales,
-        y=sorted_products_names,
-        orientation = "h"
-    )
-]
-
 chart_title = "Top Selling Products (March 2018)"
 
-layout = graph_objs.Layout(
-    title=chart_title,
-    yaxis=dict(autorange="reversed")
-)
+sorted_products = []
+sorted_sales = []
 
+for d in top_sellers:
+    sorted_products.append(d["name"])
+    sorted_sales.append(d["monthly_sales"])
 
-chart_options = {"data": data,"layout": layout}
-
-
-
-
-plotly.offline.plot(
-    chart_options,
-    filename=chart_filepath,
-    auto_open=True
-)
+plt.bar(sorted_products, sorted_sales)
+plt.title(chart_title)
+plt.xlabel("product")
+plt.ylabel("Monthly Sales (USD)")
+plt.show()
